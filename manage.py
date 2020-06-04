5d46684ce46c601f6b747cd007668fb9716900e3
John Alcher M. Doliras <johnalcherdoloiras@gmail.com>
2020-06-04 21:54:41 +0800

    Use SQLite when running development tests

--- manage.py
+++ manage.py
@@ -9,6 +9,7 @@ load_dotenv()
 
 
 def main():
+    os.environ.setdefault('ENV', 'dev')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cncs.settings')
     try:
         from django.core.management import execute_from_command_line
